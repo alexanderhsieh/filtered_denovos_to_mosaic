2d9f03f7c283b83dd16e25a6239b422e3528727f
Alexander Hsieh <alexanderhsieh7@gmail.com>
2020-06-11 10:43:14 -0400

    add step to check if required packages are installed and to install them if missing

--- EM-mosaic.v1.R
+++ EM-mosaic.v1.R
@@ -1,5 +1,5 @@
 ## check if required packages are installed; if not install them
-list.of.packages <- c("bbmle", "emdbook", "ggplot2", "tidyverse")
+list.of.packages <- c("bbmle", "emdbook", "ggplot2")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
